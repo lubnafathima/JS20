0de80f577b9d902d3d05106279fc7fae3d50574a
Lubna Fathima N <lubnafathima275@gmail.com>
2024-05-23 21:59:56 +0530

    init: complete code

---
+++ script.js
@@ -0,0 +1,17 @@
+// let arr = [1, 2, 3];
+// let newArr = [];
+
+// for (let index in arr) {
+//   if (index >= 1) {
+//     newArr[index - 1] = arr[index];
+//   }
+// }
+// console.log(arr);
+// console.log(newArr);
+
+let arr = [1, 2, 3];
+let arr2 = [4, 5];
+let newArr = [];
+newArr.push(arr + arr2);
+// newArr.push(arr2);
+console.log(arr);
